974f1a5de87d71266262d1ad6bcc58ca35a6b03b
youn <youn@melois.dev>
2025-07-16 00:20:18 +0200

    feat: add clap

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
 mod builtins;
 
 use std::fs::read_to_string;
+use std::path::PathBuf;
 
+use clap::Parser;
 use snix_eval::{
     EvalMode,
     Evaluation,
 };
 
+/// Lightweight API testing tool
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to the test file
+    file: PathBuf,
+}
+
 fn main() {
-    let code = read_to_string("tests.nix").expect("Failed to read tests.nix");
+    let args = Args::parse();
+
+    let code = read_to_string(&args.file).expect("Failed to read tests.nix");
 
     let evaluation = Evaluation::builder_impure()
         .add_builtins(builtins::builtins())
@@ -16,7 +28,7 @@ fn main() {
         .disable_import()
         .build();
 
-    let eval = evaluation.evaluate(code, None);
+    let eval = evaluation.evaluate(code, Some(args.file));
 
     if !eval.errors.is_empty() {
         for error in eval.errors {
