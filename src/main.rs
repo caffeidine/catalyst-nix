4eb682cc15d6c2dab85558f968316b55a2247b38
youn <youn@melois.dev>
2025-07-15 20:32:14 +0200

    feat: first POC

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,108 @@
+use snix_eval::EvalMode;
+use snix_eval::builtin_macros::builtins;
+
 fn main() {
-    println!("Hello, world!");
+    let code = std::fs::read_to_string("tests.nix").expect("Failed to read tests.nix");
+
+    let evaluation = snix_eval::Evaluation::builder_impure()
+        .add_builtins(extra::builtins())
+        .mode(EvalMode::Strict)
+        .disable_import()
+        .build();
+
+    let eval = evaluation.evaluate(code, None);
+
+    if !eval.errors.is_empty() {
+        for error in eval.errors {
+            println!("Error: {error:?}");
+        }
+        return;
+    }
+
+    if !eval.warnings.is_empty() {
+        for warning in eval.warnings {
+            println!("Warning: {warning:?}");
+        }
+    }
+}
+
+#[builtins]
+mod extra {
+    use std::collections::BTreeMap;
+    use std::str::FromStr;
+    use std::time::Duration;
+
+    use reqwest::Method;
+    use reqwest::blocking::Client;
+    use serde::Deserialize;
+    use serde_json::{
+        from_str,
+        from_value,
+    };
+    use snix_eval::generators::{
+        Gen,
+        GenCo,
+    };
+    use snix_eval::{
+        ErrorKind,
+        NixAttrs,
+        NixString,
+        Value,
+    };
+
+    #[derive(Debug, Deserialize)]
+    struct Params {
+        method: String,
+        url: String,
+        #[serde(default)]
+        headers: BTreeMap<String, String>,
+        timeout: u64,
+    }
+
+    #[builtin("httpRequest")]
+    pub async fn http_request(
+        co: GenCo,
+        args: Value,
+    ) -> Result<Value, ErrorKind> {
+        let (json, _ctx) = args.into_contextful_json(&co).await?;
+
+        let Params {
+            method,
+            url,
+            headers,
+            timeout,
+        } = from_value(json).expect("Failed to parse parameters");
+
+        let method = Method::from_str(&method).expect("Failed to parse method");
+
+        let http_client = Client::builder()
+            .timeout(Duration::from_millis(timeout))
+            .build()
+            .expect("Failed to build http client");
+
+        let mut request = http_client.request(method, url);
+
+        for (key, value) in headers {
+            request = request.header(key, value)
+        }
+
+        let response = request.send().expect("Failed to send http request");
+
+        let status = response.status().as_u16();
+
+        let bytes = response.bytes().expect("Failed to read response body");
+
+        let body = String::from_utf8(bytes.to_vec()).expect("Failed to convert bytes to string");
+
+        // snix_eval::Value instead of serde_json::Value but we can safely assume that
+        // if the response body is a valid JSON string, it can be parsed into a
+        // snix_eval::Value
+        let json: Value = from_str(&body).unwrap_or(Value::Null);
+
+        Ok(Value::attrs(NixAttrs::from_iter([
+            ("status", Value::Integer(i64::from(status))),
+            ("body", Value::String(NixString::from(body))),
+            ("json", json),
+        ])))
+    }
 }
---
+++ tests.nix
@@ -0,0 +1,40 @@
+with builtins;
+rec {
+  config = {
+    baseUrl = "https://jsonplaceholder.typicode.com";
+    headers = {
+      "User-Agent" = "Catalyst Test";
+      "Content-Type" = "application/json";
+    };
+  };
+
+  tests."First Test" = rec {
+    response = httpRequest {
+      method = "GET";
+      url = config.baseUrl + "/posts/1";
+      headers = config.headers;
+      timeout = 2000;
+    };
+    _ = (
+      assert response.status == 200;
+      assert response.json != null;
+      null
+    );
+  };
+
+  tests."Second Test" = rec {
+    response = httpRequest {
+      method = "POST";
+      url = config.baseUrl + "/posts";
+      headers = config.headers // {
+        "Authorization" = "Bearer " + toString tests."First Test".response.json.id;
+      };
+      timeout = 2000;
+    };
+    _ = (
+      assert response.status == 201;
+      assert response.json != null;
+      null
+    );
+  };
+}
